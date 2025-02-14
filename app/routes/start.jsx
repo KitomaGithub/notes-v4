7f5eb15623c04ee2e7657d6679d1f74b4e9f7cc6
KitomaGithub <kitomahiragashi@gmail.com>
2025-02-14 23:28:04 +0800

    test

--- app/routes/start.jsx
+++ app/routes/start.jsx
@@ -1,11 +1,19 @@
 import { useEffect } from "react";
+import { Welcome } from "../welcome/welcome";
 import { useNavigate } from "react-router";
 
-export default function Home() {
-  let navigate = useNavigate();
-  useEffect(() => {
-    navigate("/login");
-  })
+export function meta() {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
 
-  return (<></>);
+export default function Home() {
+    let navigate = useNavigate();
+    useEffect(() => {
+        navigate("/login");
+    }, [])
+
+  return <Welcome />;
 }
